6e288f00a088d64f455f132f93a8f59055150fb9
Ilari Scheinin <ilari.scheinin@gmail.com>
2015-04-22 17:03:11 -0400

    Move Canadian programming library functions to a dedicated environment.

--- canadian.R
+++ canadian.R
@@ -1,5 +1,20 @@
-infinite.eh <- function(...) { is.infinite(...) }
-numeric.eh <- function(...) { is.numeric(...) }
-na.eh <- function(...) { is.na(...) }
+# to keep the global environment clean, use a dedicated one for the functions.
+
+# first check if it already exists
+if (!exists(".canadian_programming")) {
+
+  # and if not, create it,
+  .canadian_programming <- new.env()
+
+  # add the needed functions
+  evalq({
+    infinite.eh <- function(...) { is.infinite(...) }
+    numeric.eh <- function(...) { is.numeric(...) }
+    na.eh <- function(...) { is.na(...) }
+  }, envir=.canadian_programming)
+
+  # and attach it to make the functions available
+  attach(.canadian_programming)
+}
 
 # EOF
